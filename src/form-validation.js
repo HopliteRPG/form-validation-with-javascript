3edca8de98471a37d76593f7b36082ea4ff3830c
Bill <146594930+HopliteRPG@users.noreply.github.com>
2025-07-01 16:17:22 -0400

    email input validation is working

--- src/form-validation.js
+++ src/form-validation.js
@@ -1,12 +1,40 @@
 export { formValidation };
 
+function emailInputValidation() {
+  // alert("email ran");
+  let emailInput = document.querySelector(".email-input");
+
+  // if (emailInput.validity.typeMismatch) {
+  //   alert("type mismatch");
+  //   emailInput.setCustomValidity("I am expecting an email address!");
+  //   emailInput.reportValidity();
+  //   return;
+  // } else {
+  //   emailInput.setCustomValidity("");
+  // }
+
+  emailInput.addEventListener("input", (event) => {
+    if (emailInput.validity.typeMismatch) {
+      emailInput.setCustomValidity("Submit a proper email adress");
+    } else {
+      emailInput.setCustomValidity("");
+    }
+  });
+}
+
 function postalCodeInputValidation() {
   let postalCodeInput = document.querySelector(".postal-code-input");
   postalCodeInput.setAttribute("max", "99999");
-  console.log(postalCodeInput);
-  console.log("ran");
+
+  if (postalCodeInput.validity.rangeOverflow) {
+    postalCodeInput.setCustomValidity("I am expecting an email address!");
+  } else {
+    postalCodeInput.setCustomValidity("");
+  }
 }
 
 function formValidation() {
+  let formVar = document.querySelector("form");
+  emailInputValidation();
   postalCodeInputValidation();
 }
