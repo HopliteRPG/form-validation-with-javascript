c1844f79712a2b0c5c8636c799ab13c004e59d67
Bill <146594930+HopliteRPG@users.noreply.github.com>
2025-08-27 20:09:07 -0400

    seperated emailValidation and passwordValidation into their own functions

--- src/form-validation.js
+++ src/form-validation.js
@@ -2,9 +2,25 @@ export { fullFormValidation };
 const form = document.querySelector("form");
 
 function fullFormValidation() {
+  emailValidation();
+  passwordValidation();
+}
+
+function emailValidation() {
+  const emailInput = document.querySelector(".email-input");
+
+  emailInput.addEventListener("input", (event) => {
+    if (emailInput.validity.typeMismatch) {
+      emailInput.setCustomValidity("Submit a proper email address");
+    } else {
+      emailInput.setCustomValidity("");
+    }
+  });
+}
+
+function passwordValidation() {
   const passwordInput = document.querySelector(".password-input");
   const passwordConfInput = document.querySelector(".password-confirm-input");
-
   passwordInput.addEventListener("input", (event) => {
     if (passwordInput.value !== passwordConfInput.value) {
       passwordInput.setCustomValidity("Passwords do not match!");
